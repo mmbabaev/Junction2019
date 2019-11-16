719c0fda25b2de9d8606d6e75f9050f37349abf6
Mikhail Babaev <mbabaev@stream.ru>
2019-11-16 17:15:25 +0200

    init commit 2

--- Pods/Target Support Files/Pods-Node Demos-Node DemosUITests/Pods-Node Demos-Node DemosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Node_Demos_Node_DemosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Node_Demos_Node_DemosUITests
-@end
--- Pods/Target Support Files/Pods-Node Demos/Pods-Node Demos-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Node_Demos : NSObject
-@end
-@implementation PodsDummy_Pods_Node_Demos
-@end
